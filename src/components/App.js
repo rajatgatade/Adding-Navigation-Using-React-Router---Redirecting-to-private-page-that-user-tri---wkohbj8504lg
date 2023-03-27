49dec512691097160111098f5a8be515feae4189
rajatgatade <97111225+rajatgatade@users.noreply.github.com>
2023-03-27 15:58:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,60 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link, useLocation } from "react-router-dom";
+import "../styles/App.css";
 
-class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+function Home() {
+  return <h2>You are home.</h2>;
 }
 
+function About() {
+  return <h2>You are on the about page.</h2>;
+}
+
+function NoMatch() {
+  return <h2>No match.</h2>;
+}
+
+function LocationDisplay() {
+  const location = useLocation();
+
+  return (
+    <div data-testid="location-display">
+      <h3 data-testid="location">{location.pathname}</h3>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <div id="main">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+
+        <LocationDisplay />
+      </div>
+    </Router>
+  );
+}
 
 export default App;
